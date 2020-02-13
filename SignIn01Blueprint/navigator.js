e3c3e7e838522d743c936dd47510caa20df2bf48
tanduong <tanduong@live.com>
2020-02-13 06:31:35 -0600

    PLAT 1223

--- SignIn01Blueprint/navigator.js
+++ SignIn01Blueprint/navigator.js
@@ -4,7 +4,7 @@ import {SignIn1Container} from './screens/signIn1/signIn1.container';
 
 import Home from './screens/';
 
-export const SignIn01BlueprintNavigator = createStackNavigator(
+export default SignIn01BlueprintNavigator = createStackNavigator(
   {
     Home: {screen: Home},
     SignIn1: {screen: SignIn1Container},
